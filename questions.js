8077bb20d56dccd466c9cd468cbc1c34398b9f94
sunil2059 <88591279+sunil2059@users.noreply.github.com>
2023-09-08 16:26:14 +0545

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions =[
+    {
+        numb: 1,
+        question: "A 73-year-old man is brought to the emergency department because of fever, malaise, dyspnea, and a productive cough with purulent sputum for the past day. His temperature is 39.2°C (102.6°F). Pulmonary examination shows crackles over the right upper lung field. Sputum Gram stain shows gram-positive cocci. Despite the appropriate treatment, the patient dies 5 days later. At autopsy, gross examination shows that the right lung has a pale, grayish-brown appearance and a firm consistency. Microscopic examination of the tissue is most likely to show which of the following?",
+        answer: "Fibrinopurulent leukocytic exudate with lysed erythrocytes",
+        options:[
+            "Fibrinopurulent leukocytic exudate with lysed erythrocytes",
+            "Fibrinous exudate with erythrocytes, leukocytes, and bacteria",
+            "Resorbed exudate with aerated alveoli",
+            "Neutrophilic infiltrate in the bronchiolar walls and adjacent alveoli"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A 20 year old man complains of dark urine and decreased urine output for 2 days. He had skin infection resolved with antibiotics. Physical examination shows periorbital edema bilaterally. Most likely finding is:",
+        answer: "Elevated Anti-DNase B titer",
+        options:[
+            "Mesangial IgA deposition",
+            "Decreased platelet count",
+            "Elevated Anti-DNase B titer",
+            "Impaired glutathione regeneration"
+        ]
+    },
+    {
+        numb: 3,
+        question: "A 45-year-old man is brought to the emergency department because of severe abdominal pain for the past 2 hours. He has a 2-year history of burning epigastric pain that gets worse with meals. His pulse is 120/min, respirations are 22/min, and blood pressure is 60/40 mm Hg. Despite appropriate lifesaving measures, he dies. At autopsy, examination shows erosion of the right gastric artery. Perforation of an ulcer in which of the following locations most likely caused this patient's findings?",
+        answer: "Lesser curvature of the stomach",
+        options:[
+            "Greater curvature of the stomach",
+            "Anterior duodenum",
+            "Posterior duodenum",
+            "Lesser curvature of the stomach"
+        ]
+    },
+    {
+        numb: 4,
+        question: "A 23 years old woman comes to the physician because of a 2 month history of diarrhea, flatulence and fatigue. She is having about 4 episodes of loose stool daily that have oily appearance.Symptoms worsens on eating and also complains of itchy rash on elbows and knees. Most likely finding is ",
+        answer: "HLA-DQ2 serotype",
+        options:[
+            "Low fecal elastase level",
+            "HLA-DQ2 serotype",
+            "PAS positive intestinal macrophages",
+            "Elevated exhaled hydrogen concentration"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A 72 years old man is brought to emergency department after episode of hemoptysis with history of chronic cough (copious sputum). Physical examination shows digital clubbing and x-ray shows tram track opacities in lower lung fields. What is the most likely diagnosis?",
+        answer: "Bronchiectasis",
+        options:[
+            "Emphysema",
+            "Lung cancer",
+            "Bronchiectasis",
+            "Aspiration pneumonia"
+        ]
+    },
+    {
+        numb: 6,
+        question: "A 49-year-old woman comes to the physician for a routine health maintenance examination and Pap smear. Her last Pap smear was 3 years ago and showed no abnormalities. She has smoked 1 pack of cigarettes daily for 32 years. She takes no medications apart from a multivitamin supplement. Physical examination shows no breast masses. The external genitalia, vagina, and cervix appear normal. The uterus and adnexa are normal to palpation. A Pap smear shows moderate epithelial dysplasia of half of the basal epithelium and several koilocytes at the squamocolumnar junction. Without treatment, this patient is at greatest risk of developing which of the following? ",
+        answer: "Postrenal azotemia",
+        options:[
+            "Superior vena cava syndrome",
+            "Pericardial effusion",
+            "Paraneoplastic polycythemia",
+            "Postrenal azotemia"
+        ]
+    },
+    {
+        numb: 7,
+        question: "A 67-year-old man comes to the emergency department because of retrosternal chest pressure and shortness of breath for 4 hours. The symptoms started while he was walking to work and have only minimally improved with rest. He has a history of type 2 diabetes mellitus. He has smoked one pack of cigarettes daily for 35 years. He appears uncomfortable. His pulse is 95/min. Serum studies show a normal troponin concentration. An ECG shows no abnormalities. Which of the following is the most likely underlying cause of this patient's symptoms? ",
+        answer: "Disruption of an atherosclerotic plaque with a nonocclusive coronary artery thrombus",
+        options:[
+            "Coronary artery occlusion due to transient increase in vascular tone",
+            "Disruption of an atherosclerotic plaque with a nonocclusive coronary artery thrombus",
+            "Stable atherosclerotic plaque with 85% coronary artery occlusion",
+            "Infarction and rupture of a papillary muscle"
+        ]
+    },
+    {
+        numb: 8,
+        question: "A 65-year-old man comes to the physician because of progressive abdominal distension and swelling of his legs for 4 months. He has a history of ulcerative colitis. Physical examination shows jaundice. Abdominal examination shows shifting dullness and dilated veins in the periumbilical region. This patient's abdominal findings are most likely caused by increased blood flow in which of the following vessels?",
+        answer: "Superior epigastric vein",
+        options:[
+            "Superior rectal vein",
+            "Superior mesenteric vein",
+            "Superior epigastric vein",
+            "Splenic vein"
+        ]
+    },
+    {
+        numb: 9,
+        question: "A 12-year-old girl is brought to the physician because of a 2-hour history of severe epigastric pain, nausea, and vomiting. Her father has a history of similar episodes of abdominal pain and developed diabetes mellitus at the age of 30 years. Abdominal examination shows guarding and rigidity. Ultrasonography of the abdomen shows diffuse enlargement of the pancreas; no gallstones are visualized. Which of the following is the most likely underlying cause of this patient's condition? ",
+        answer: "Premature activation of trypsinogen",
+        options:[
+            "Premature activation of trypsinogen",
+            "Defective bilirubin glucuronidation",
+            "Defected elastase inhibitor",
+            "Impaired cellular copper transport"
+        ]
+    },
+    {
+        numb: 10,
+        question: "A 46-year-old woman from Gabon is admitted to the hospital because of tarry black stools and epigastric pain for 2 weeks. The epigastric pain is briefly relieved after eating but worsens within 1–2 hours. She has no history of serious illness and takes no medications. Physical examination shows no abnormalities. Fecal occult blood test is positive. Esophagogastroduodenoscopy shows a bleeding duodenal ulcer. Biopsy of the gastric antrum and body is performed. Microscopic examination of the biopsy specimens is most likely to show which of the following? ",
+        answer: "Spiral-shaped, flagellated gram-negative rods",
+        options:[
+            "Teardrop-shaped multinucleated trophozoites",
+            "Dimorphic budding yeasts with pseudohyphae",
+            "Spiral-shaped, flagellated gram-negative rods",
+            "Irregularly drumstick-shaped gram-positive rods"
+        ]
+    }
+]
